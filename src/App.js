fb0bae450706a4b35a1e6d03039ec125852c6561
OMAGENE Sheyi Matthew <sheyions4real@yahoo.co.uk>
2022-02-04 14:49:43 +0100

    collection items and data completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
 
 import './pages/homepage/homepage.styles.scss';
 
+
 import HomePage from './pages/homepage/homepage.component';
-import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 
 
- const HatsPage =(props)=>{
-  console.log(props)
-   return(
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
- }
+
+//  const HatsPage =(props)=>{
+//   console.log(props)
+//    return(
+//   <div>
+//     <h1>Hats Page</h1>
+//   </div>
+// );
+//  }
 
 
 function App() {
@@ -24,7 +27,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   
