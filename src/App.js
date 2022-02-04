bdbd1bec82d8f587ba5241d3a39dcd8abe60d0e4
OMAGENE Sheyi Matthew <sheyions4real@yahoo.co.uk>
2022-02-04 18:37:53 +0100

    Added Header Component to Home

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './pages/homepage/homepage.styles.scss';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 
@@ -25,6 +26,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
